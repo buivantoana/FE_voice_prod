35449251bbfd5cf784895f9193a42def8b99ec17
buivantoana <106064775+buivantoana@users.noreply.github.com>
2025-04-13 22:38:27 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const url_auth = "https://dev-koc.gmv.vn";
-export const url_voice = "https://dev.ttsopenai.zeezoo.mobi";
+export const url_voice = "https://dev.ttsopenai.zeezoo.mobi/api";
+  ";
 export const path_redirect = "https://ai.gmv.vn/auth/callback";
