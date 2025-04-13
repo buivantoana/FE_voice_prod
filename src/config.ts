1af1667c281a316c5d608c7df6ae88a0737277c5
buivantoana <toanbui219@gmail.com>
2025-04-13 22:46:21 +0700

    .

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const url_auth = "https://dev-koc.gmv.vn";
 export const url_voice = "https://dev.ttsopenai.zeezoo.mobi/api";
-  ";
 export const path_redirect = "https://ai.gmv.vn/auth/callback";
